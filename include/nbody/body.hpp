16494d56a4bf258f704823fdb908b6a83ba4b8af
Benoit Landry <landry@mit.edu>
2014-01-27 16:54:01 -0500

    body interface

--- include/nbody/body.hpp
+++ include/nbody/body.hpp
@@ -1,37 +1,16 @@
-  // velocity
-  // acceleration
-  // masses
-  // output format
-  // output as text file
 
 namespace nbody {
   class Body {
-    vector _velocity;
-    vector _acceleration;
-    vector _mass;
-    Body // constructor
-    ~Body // free
+    // state = < x, y, dx, dy >
+    vector _state;
+    double _mass;
     public:
-      const void velocity() {};
-      const void acceleration() {};
-      const void mass() {};
-      void &velocity( vector velocity ) {};
-      void &acceleration( vector acceleration ) {};
-      void &mass( vector mass ) {};
-      const void printBody() {};
+    Body();
+    ~Body();
+    
+    const void velocity() {};
+    const void mass() {};
+    void &mass( vector mass ) {};
+    const void printBody() {};
   };
-}
-
-
-template <typename T>
-class Vector3 {
-  T, _x, _y, _z;
-};
-
-// we are treating these objects as immutable,
-// This speeds up the calculation
-template <typename T>
-inline const Vector3<T> operator+( const Vector3<T> &a,
-                                   const Vector3<T> &a  ) {
-  return Vector3<T> { a.x() + b.x(), a.y() + b.y(), a.z() + b.z() };
 }
