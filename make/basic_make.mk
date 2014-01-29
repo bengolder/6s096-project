2297c0c5cfbc24037bbecf237487c2baab4e432b
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-01-29 17:32:01 -0500

    added make commands for viz, recompiled body

--- make/basic_make.mk
+++ make/basic_make.mk
@@ -10,12 +10,15 @@ DEBUGFLAGS := -g -O0 -D _DEBUG
 RELEASEFLAGS := -O2 -D NDEBUG
 
 LDFLAGS := -Linstall/lib \
-	-lm -lpthread
+	-L/usr/local/lib \
+	-lm -lpthread \
+	-lglut
 
 INCLUDEFLAGS := -I include/$(PROG) \
 				 -I install/lib \
 				 -I third_party/gtest \
-				 -I third_party/gtest/include
+				 -I third_party/gtest/include \
+				 -I /usr/local/include/GL/
 
 # $@ is the current target
 # $^ is all the prerequisites
@@ -33,6 +36,9 @@ install/test/system-test.x: install/include/system-test.o \
 	ar rcs install/lib/libnbody.a install/include/body.o install/include/system.o
 	$(CXX) -o $@ $< $(LDFLAGS) -lnbody
 
+install/test/viz-test.x: install/include/window.o
+	$(CXX) -o $@ $< $(LDFLAGS) 
+
 # object files
 install/include/body.o: src/body.cpp
 	$(CXX) -c -o $@ $< $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
@@ -44,9 +50,11 @@ install/include/system.o: src/system.cpp
 install/include/system-test.o: test/system-test.cpp
 	$(CXX) -c -o $@ $< $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
 
+install/include/window.o: src/window.cpp
+	$(CXX) -c -o $@ $< $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
 
 # compiling targets
-.PHONY : all body body-test system system-test clean
+.PHONY : all body body-test system system-test clean test viz
 
 all: install/test/body-test.x
 
@@ -60,6 +68,13 @@ system: install/test/system-test.x
 system-test: install/test/system-test.x
 	install/test/body-test.x
 	install/test/system-test.x
+
+viz: install/test/viz-test.x
+	install/test/viz-test.x
+
+viz-test: install/test/viz-test.x
+
+test: system-test
 
 clean:
 	rm -f install/test/*.x install/lib/*.a install/include/*.o
