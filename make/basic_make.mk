6d127d5c67fefc2fa57fbfd475f33cabd4182232
Benjamin Golder <benajmin.j.golder@gmail.com>
2014-01-29 10:19:46 -0500

    rewrote make file to allow for separate unit tests

--- make/basic_make.mk
+++ make/basic_make.mk
@@ -17,30 +17,49 @@ INCLUDEFLAGS := -I include/$(PROG) \
 				 -I third_party/gtest \
 				 -I third_party/gtest/include
 
-# create executable
+# $@ is the current target
+# $^ is all the prerequisites
+# $< is the first prerequisite
+
+# executables and archives
 install/test/body-test.x: install/include/body-test.o \
-					      install/lib/libnbody.a
-	$(CXX) -o $@ $^ $(LDFLAGS)
-	# $@ is the current target
-	# $^ is all the prerequisites
+						  install/include/body.o
+	ar rcs install/lib/libnbody.a install/include/body.o
+	$(CXX) -o $@ $< $(LDFLAGS) -lnbody
 
-# create archive
-install/lib/libnbody.a: install/include/body.o 
-	ar rcs $@ $^
+install/test/system-test.x: install/include/system-test.o \
+							install/include/system.o \
+							install/include/body.o
+	ar rcs install/lib/libnbody.a install/include/body.o install/include/system.o
+	$(CXX) -o $@ $< $(LDFLAGS) -lnbody
 
-# create object files
+# object files
 install/include/body.o: src/body.cpp
-	$(CXX) -c -o $@ $^ $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
+	$(CXX) -c -o $@ $< $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
 install/include/body-test.o: test/body-test.cpp
-	$(CXX) -c -o $@ $^ $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
+	$(CXX) -c -o $@ $< $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
+
+install/include/system.o: src/system.cpp
+	$(CXX) -c -o $@ $< $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
+install/include/system-test.o: test/system-test.cpp
+	$(CXX) -c -o $@ $< $(INCLUDEFLAGS) $(CXXFLAGS) $(DEBUGFLAGS)
+
 
 # compiling targets
+.PHONY : all body body-test system system-test clean
+
 all: install/test/body-test.x
 
 body: install/test/body-test.x
 
 body-test: install/test/body-test.x
 	install/test/body-test.x
+
+system: install/test/system-test.x
+
+system-test: install/test/system-test.x
+	install/test/body-test.x
+	install/test/system-test.x
 
 clean:
 	rm -f install/test/*.x install/lib/*.a install/include/*.o
---
+++ test/system-test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <nbody/system.hpp>
+#include <nbody/body.hpp>
+
+int main() {
+
+  std::cout << "Body test:\n";
+  Body body = Body(5);
+  body.print();
+
+  std::cout << "System test:\n";
+  System system = System();
+  Body body1 = Body(10);
+  Body body2 = Body(15);
+  Body body3 = Body(17);
+  system.add_body(body1);
+  system.add_body(body2);
+  system.add_body(body3);
+  system.print();
+
+  std::cout << "Taking one step forward in time:\n";
+  system.update(0.01);
+  system.print();
+
+  return 0;
+}
