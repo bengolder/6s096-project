d60b90af99224d2d77c9cb178146cacbaf01f196
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-01-31 15:39:06 -0500

    glew included, OpenGL loading nicely

--- test/window-test.cpp
+++ test/window-test.cpp
@@ -1,3 +1,5 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
 
@@ -6,6 +8,11 @@ int main() {
   SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
   SDL_GLContext context = SDL_GL_CreateContext(window);
   SDL_Event windowEvent;
+  glewExperimental = GL_TRUE;
+  glewInit();
+  GLuint vertexBuffer;
+  glGenBuffers(1, &vertexBuffer);
+  printf("%u\n", vertexBuffer);
   while (true) {
     if (SDL_PollEvent(&windowEvent)) {
       if (windowEvent.type == SDL_QUIT) break;
