2e95880bed59fa9f5d08494cd3ebd18f41797f24
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-01-31 15:30:22 -0500

    window test with SDL now makes a window

--- test/window-test.cpp
+++ test/window-test.cpp
@@ -1,11 +1,20 @@
 #include <SDL.h>
+#include <SDL_opengl.h>
 
-int main()
-{
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    SDL_Delay(1000);
-
-    SDL_Quit();
-    return 0;
+int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+  SDL_Event windowEvent;
+  while (true) {
+    if (SDL_PollEvent(&windowEvent)) {
+      if (windowEvent.type == SDL_QUIT) break;
+      if (windowEvent.type == SDL_KEYUP &&
+              windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
+    }
+    SDL_GL_SwapWindow(window);
+  }
+  SDL_GL_DeleteContext(context);
+  SDL_Quit();
+  return 0;
 }
